db1d2de46322fadb5a1c05ae9f231a48b8bcc0da
opilane <opilane@HP-CZC2349HTD>
2023-04-19 10:31:31 +0300

    Add project files.

---
+++ MVVM/MVVM/ViewModels/FriendsListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM.ViewModels
+{
+    public class FriendsListViewModel
+    {
+        public ObservableCollection<FriendViewModel> Friends { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand CreateFriendCommand { protected get; set; }
+        public ICommand DeleteFriendCommand { protected get; set; }
+        public ICommand SaveFriendCommand { protected get; set; }
+        public ICommand BackCommand { protected get; set; }
+        FriendViewModel selectedFriend;
+        public INavigation Navigation { get; set; }
+
+        public FriendsListViewModel()
+        {
+            Friends = new ObservableCollection<FriendViewModel>();
+            CreateFriendCommand = new Command(CreateFriend);
+            DeleteFriendCommand = new Command(DeleteFriend);
+            SaveFriendCommand = new Command(SaveFriend);
+            BackCommand = new Command(Back);
+        }
+        public FriendViewModel SelectedFriend
+        {
+            get { return selectedFriend; }
+            set
+            {
+                if (selectedFriend != value) { FriendViewModel tempFriend = value; selectedFriend = null; OnPropertyChanged("SelectedFriend"); Navigation.PushAsync(new FriendPage(tempFriend)); }
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        private void CreateFriend()
+        {
+            Navigation.PushAsync(new FriendPage(new FriendViewModel() { listViewModel=}))
+        }
+    }
+}
